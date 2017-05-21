8199dfbb94b2300c48f46744a56a80c9c75a7cb9
Trevor Ford <info@trvrfrd.net>
2017-05-20 22:42:56 -0400

    rails g scaffold Cart + rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425172411) do
+ActiveRecord::Schema.define(version: 20170521021934) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
