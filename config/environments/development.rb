1deeaee9f65b09dc1e3893837877b1a09eb0a680
Trevor Ford <info@trvrfrd.net>
2017-05-24 16:59:08 -0400

    I swear I already did the web console whitelisting thing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Allow web console on Vagrant dev server
+  config.web_console.whitelisted_ips = '10.0.2.2'
 end
