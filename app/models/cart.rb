ef2a2abe3b8d12712a81e03e796964ea6088d2ca
Trevor Ford <info@trvrfrd.net>
2017-05-25 13:16:56 -0400

    add unit tests for Cart#add_product, #total_price

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,6 +3,8 @@ class Cart < ApplicationRecord
 
   def add_product(product)
     current_item = line_items.find_or_initialize_by(product_id: product.id)
+    # this won't work properly when calling multiples times without saving the
+    # cart in between calls. is that bad?
     current_item.quantity += 1 unless current_item.new_record? # default = 1
     current_item.price = product.price
     current_item
--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @cart = carts(:one)
+    @product = products(:ruby)
+  end
+
+  test '#add_product adds a product via line_items' do
+    assert_difference('@cart.line_items.size', 1) do
+      @cart.add_product(@product)
+    end
+    assert_equal @product, @cart.line_items.last.product
+  end
+
+  test '#add_product copies product price to line item' do
+    line_item = @cart.add_product(@product)
+    assert_equal @product.price, line_item.price
+  end
+
+  test '#add_product updates quantity for duplicate items' do
+    line_item = @cart.add_product(@product)
+    assert_equal 1, line_item.quantity
+    assert_equal @product, line_item.product
+    # this is smelly, should #add_product itself do the saving?
+    @cart.save!
+    line_item = @cart.add_product(@product)
+    assert_equal 2, line_item.quantity
+    assert_equal @product, line_item.product
+  end
+
+  test '#total_price sums line item prices' do
+    assert_equal products(:one).price, @cart.total_price
+    @cart.line_items.create!(product: products(:two))
+    assert_equal products(:one).price + products(:two).price, @cart.total_price
+  end
 end
