bfd023ff31c73c579f3250abc46417f79f6674fa
Trevor Ford <info@trvrfrd.net>
2017-05-25 17:49:30 -0400

    update LineItem#total_price, add price validation, set price from Product by default

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,7 +2,18 @@ class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
 
+  # New LineItems are mostly created via Cart#add_product, which also sets the
+  # price from the associated Product. This is just an extra line of defense.
+  before_validation :set_price_from_product, on: :create
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
   def total_price
-    product.price * quantity
+    price * quantity
+  end
+
+  private
+
+  def set_price_from_product
+    self.price = product.price
   end
 end
