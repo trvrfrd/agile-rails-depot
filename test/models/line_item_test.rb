6c535f00d7a89e09ad165daa20ba2793196dde1d
Trevor Ford <info@trvrfrd.net>
2017-05-25 17:51:36 -0400

    update LineItem fixtures, unit tests for LineItem#total_price and more!

--- test/models/line_item_test.rb
+++ test/models/line_item_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class LineItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup { @line_item = line_items(:one) }
+
+  test 'sets price from product by default on create' do
+    item = LineItem.create!(product: products(:one), cart: carts(:one))
+    assert_equal products(:one).price, item.price
+  end
+
+  test '#total_price accounts for price and quantity' do
+    @line_item.quantity = 99
+    assert_equal 99 * @line_item.price, @line_item.total_price
+  end
+
+  test '#total_price uses line item price rather than product price' do
+    @line_item.price = 666.00
+    assert_equal @line_item.price, @line_item.total_price
+    assert_not_equal @line_item.product.price, @line_item.total_price
+  end
 end
