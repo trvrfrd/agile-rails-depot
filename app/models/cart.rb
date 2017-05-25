bbf4fe24c7779ff34dff22700ebdabd84361c359
Trevor Ford <info@trvrfrd.net>
2017-05-25 10:47:33 -0400

    Cart#add_product: update price from product + refactoring

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,12 +2,9 @@ class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
 
   def add_product(product)
-    current_item = line_items.find_by(product_id: product.id)
-    if current_item.present?
-      current_item.quantity += 1
-    else
-      current_item = line_items.build(product_id: product.id)
-    end
+    current_item = line_items.find_or_initialize_by(product_id: product.id)
+    current_item.quantity += 1 unless current_item.new_record? # default = 1
+    current_item.price = product.price
     current_item
   end
 
