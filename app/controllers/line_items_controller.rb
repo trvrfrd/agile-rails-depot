46fa3341165cc35f6e61c3d6245d21886200e2f9
Trevor Ford <info@trvrfrd.net>
2017-07-07 12:03:01 -0400

    add authorize filter, update controllers + tests

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,7 @@
 class LineItemsController < ApplicationController
   include CurrentCart
 
+  skip_before_action :authorize
   before_action :set_line_item, only: [:show, :edit, :update, :destroy, :decrement]
   before_action :set_cart, only: [:create, :destroy, :decrement]
 
