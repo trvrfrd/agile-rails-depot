6ce65c3cf9706b4c276902a62b809696e497cbc3
Trevor Ford <info@trvrfrd.net>
2017-07-07 17:50:10 -0400

    add test for not being able to delete last admin user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "shouldn't destroy last user" do
+    assert_equal 2, User.count
+    User.last.destroy
+    assert_equal 1, User.count
+    assert_raise User::Error do
+      User.last.destroy
+    end
+    assert_equal 1, User.count
+  end
 end
